abd2525d95565c232a1e6ced61b6359d55bb8492
Ruslan <ruslan.gcnto@yandex.kz>
2025-04-17 21:35:29 +0500

    create react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import PostList from "./components/PostList";
+import PostFilter from "./components/PostFilter";
+import PostForm from "./components/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {'id': 1, 'title': 'Post 1 title', 'body': 'Text 1121'},
+    {'id': 2, 'title': 'VR 2', 'body': 'Text gbg'},
+    {'id': 3, 'title': 'GEMS 2', 'body': 'Text ewers'},
+    {'id': 4, 'title': 'Zapis', 'body': 'Zapis text'}
+  ]);
+
+  const [filter, setFilter] = useState({sort: '', query: ''});
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+    }
+    return posts;
+  }, [filter.sort, posts]);
+  
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => 
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPosts]);
+
+  
+  const createPost = (create) => {
+    setPosts(create);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost} />
+
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      {
+        sortedAndSearchedPosts.length
+          ?
+          <PostList posts={sortedAndSearchedPosts} />
+          :
+          <h1 style={{textAlign: 'center'}}>Записи не найдены!</h1>
+      }
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import MyButton from "./UI/button/MyButton";
+
+const PostItem = (props) => {
+
+	return (
+		<div className="post">
+			<div className="post__content">
+				<strong>{props.post.id}. {props.post.title}</strong>
+				<div>
+					{props.post.body}
+				</div>
+			</div>
+			<div className="post__btns">
+				<MyButton>
+					Открыть
+				</MyButton>
+				<MyButton>
+					Удалить
+				</MyButton>
+			</div>
+		</div>
+	);
+};
+
+export default PostItem;
